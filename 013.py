f66264691258099177a1ad6ef3c722346d8b8056
weiyu <244128764@qq.com>
2020-01-03 18:40:56 +0800

    012

---
+++ 013.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: ShidongDu time:2020/1/3
+'''
+Title: 罗马数字转整数
+例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+
+'''
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         num_dict = {'I': 1, 'IV': 3, 'V': 5, 'IX': 8, 'X': 10, 'XL': 30, 'L': 50, 'XC': 80, 'C': 100, 'CD': 300, 'D': 500,
+#              'CM': 800, 'M': 1000}
+#         sum = 0
+#         for i in range(len(s)):
+#             if i<=len(s)-2 and num_dict[s[i]] < num_dict[s[i+1]]:
+#                 sum -= num_dict[s[i]]
+#             else:
+#                 sum += num_dict[s[i]]
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {'I':1, 'IV':3, 'V':5, 'IX':8, 'X':10, 'XL':30, 'L':50, 'XC':80, 'C':100, 'CD':300, 'D':500, 'CM':800, 'M':1000}
+        return sum(d.get(s[max(i-1, 0):i+1], d[n]) for i, n in enumerate(s))
+
+        return sum
+if __name__ == "__main__":
+    solution = Solution()
+    sum = solution.romanToInt('XXX')
+    print(sum)
