5b24f32a27958d93b497ae6ae618cade993ed3f4
weiyu <244128764@qq.com>
2020-01-14 10:08:14 +0800

    2020年1月14日更新

--- 24. 两两交换链表中的节点.py
+++ 24. 两两交换链表中的节点.py
@@ -19,6 +19,61 @@ class ListNode:
         self.val = x
         self.next = None
 
-class Solution:
+# 分析：我这里借用了一个数组来存储临时的链表并精进行转换，时间复杂度为o(n)，遍历了整个列表
+# 空间复杂度o(1)，因为我只借用了大小为3的列表空间。
+# class Solution:
+#     def swapPairs(self, head: ListNode) -> ListNode:
+#         # 新建边缘节点r
+#         r = ListNode(-1)
+#         if(head and head.next):
+#             r.next = head.next
+#         else:
+#             return head
+#
+#         # 新建标记节点p
+#         p = ListNode(-1)
+#         p.next = head
+#         while(p.next and p.next.next and p.next.next.next):
+#             tmp_list = [p.next, p.next.next, p.next.next.next]
+#             p.next = tmp_list[1]
+#             tmp_list[1].next = tmp_list[0]
+#             tmp_list[0].next = tmp_list[2]
+#             p = tmp_list[0]
+#         if (p.next and p.next.next) and not p.next.next.next:
+#             tmp_list = [p.next, p.next.next]
+#             p.next = tmp_list[1]
+#             tmp_list[1].next = tmp_list[0]
+#             tmp_list[0].next = None
+#         return r.next
+
+# 递归的写法
+# 这个傻逼递归的原理我还没懂！
+class Solution():
     def swapPairs(self, head: ListNode) -> ListNode:
+        if(head == None or head.next == None):
+            return head
+        p = head.next
+        head.next = self.swapPairs(p.next)
+        p.next = head
+        return p
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    a = ListNode(6)
+    a.next = ListNode(7)
+    b = a.next
+    b.next = ListNode(8)
+    c = b.next
+    c.next = ListNode(9)
+    d = c.next
+    d.next = ListNode(10)
+    e = d.next
+
+    head = solution.swapPairs(a)
+    while(head):
+        print(head.val)
+        head = head.next
+
+
         
