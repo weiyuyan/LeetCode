f87d06d11ffb286f33c920056df0ba18da7c48bf
微雨 <244128764@qq.com>
2020-01-29 11:15:48 +0800

    2020年1月29日更新

--- Search/78.Subsets.py
+++ Search/78.Subsets.py
@@ -24,6 +24,27 @@ Output:
 '''
 # 回溯法
 from typing import List
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         res = [[]]
+#         for i in nums:
+#             res = res + [[i]+num for num in res]
+#         return res
+
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        pass
+        res = []
+        n = len(nums)
+        def helper(i, tmp):
+            res.append(tmp)
+            for j in range(i, n):
+                helper(j+1, tmp+[nums[j]])
+        helper(0, [])
+        return res
+
+
+solution = Solution()
+res = solution.subsets([1,2,3])
+print(res)
+
