b7afe5b8eb485baa7912272d555debb98d68cafe
微雨 <244128764@qq.com>
2020-02-15 00:55:12 +0800

    2020年2月15日更新

--- 剑指offer/面试题17. 打印从1到最大的n位数.py
+++ 剑指offer/面试题17. 打印从1到最大的n位数.py
@@ -16,4 +16,28 @@ n 为正整数
 '''
 from typing import List
 class Solution:
+    # def printNumbers(self, n: int) -> List[int]:
+    #     tmp_res = ['0']    # 这里放的是字符串类型的结果
+    #     res = []    # 这里放的是数值类型的结果
+    #     # if n == 1:  return [int(_) for _ in tmp_res]
+    #     if n <= 0:  return []
+    #
+    #     def dfs(tmp_res, depth):
+    #         if depth == 0:    return
+    #         next_tmp_res = []
+    #         for i in tmp_res:
+    #             for j in range(10):
+    #                 next_tmp_res.append(i+str(j))
+    #         print(next_tmp_res)
+    #         dfs(next_tmp_res, depth-1)
+    #
+    #     dfs(tmp_res, 2)
+
+class Solution:
     def printNumbers(self, n: int) -> List[int]:
+        return [i for i in range(1, 10 ** n)]
+
+
+solution = Solution()
+res = solution.printNumbers(1)
+# print(res)
